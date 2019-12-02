eca75c5989e44ae42e22afc7d191e9187d33e57a
HolyPi <joanelly2018@gmail.com>
2019-12-02 14:32:09 -0800

    fix3

--- app.py
+++ app.py
@@ -21,18 +21,6 @@ def video_url_creator(id_lst):
         videos.append(video)
     return videos
 
-# @app.route('/')
-# def index():
-    # """Return homepage."""
-    # return render_template('home.html', msg='Flask is Cool!!')
-
-
-# OUR MOCK ARRAY OF PROJECTS
-# playlists = [
-#     { 'title': 'Cat Videos', 'description': 'Cats acting weird' },
-#     { 'title': '80\'s Music', 'description': 'Don\'t stop believing!' }
-# ]
-
 @app.route('/')
 def playlists_index():
     """Show all playlists."""
